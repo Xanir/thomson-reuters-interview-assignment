916e0bc28c91e76291ebed693c237b81153b0912
Enzey <dev@enzey.net>
2025-03-17 16:08:21 -0400

    Complete UX of interface

--- src/atm.tsx
+++ src/atm.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './atm.css'
 import atmLogo from '/atm_sign.png'
+import atmSystemsLogo from '/systems.png'
+
 import ATMCardTypeMaestra from './Components/atmCardTypeMaestra.tsx'
 import ATMCardTypeMasterCard from './Components/atmCardTypeMasterCard.tsx'
 import ATMCardTypePlus from './Components/atmCardTypePlus.tsx'
@@ -25,7 +27,40 @@ function App() {
           <ATMCardTypePlus active={false}/>
           <ATMCardTypeVisa active={false}/>
         </div>
-        <div className="atm-buttons">
+        <div className="atm-interface">
+          <div className="atm-left">
+            <div className="atm-buttons">
+              <button />
+              <button />
+              <button />
+              <button />
+            </div>
+            <div className="atm-buttons-dash">
+              <div className="atm-dash" />
+              <div className="atm-dash" />
+              <div className="atm-dash" />
+              <div className="atm-dash" />
+            </div>
+          </div>
+          <div className="atm-center">
+            <div className="atm-screen">
+            </div>
+            <img className="systemsLogo" src={atmSystemsLogo} alt="systems" />
+          </div>
+          <div className="atm-right">
+            <div className="atm-buttons-dash">
+              <div className="atm-dash" />
+              <div className="atm-dash" />
+              <div className="atm-dash" />
+              <div className="atm-dash" />
+            </div>
+            <div className="atm-buttons">
+              <button />
+              <button />
+              <button />
+              <button />
+            </div>
+            </div>
         </div>
       </div>
     </div>
